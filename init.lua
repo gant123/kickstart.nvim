95e35d4b5901d88bef3ebb4becf1b8419dc78d87
Robert Gant <demontegant@gmail.com>
2024-12-28 22:53:15 -0600

    fix: Correct comment typo in init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- Set <space> as the leader key
--- See `:help mapleader`
+-- See `:help mapleader`fdfdfd
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
